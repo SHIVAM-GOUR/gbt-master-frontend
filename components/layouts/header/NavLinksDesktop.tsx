8b82fa93936be21468e2441040f260ef2fcf677d
riyagarg2711 <riya011172@gmail.com>
2025-09-18 16:25:07 +0530

    dark theme

--- components/layouts/header/NavLinksDesktop.tsx
+++ components/layouts/header/NavLinksDesktop.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ThemeSwitch from "./ThemeSwitch";
 
 export default function NavLinksDesktop({ isScrolled }) {
   const linkClasses = `font-bold text-base border-b-2 border-transparent hover:border-current transition duration-300 pb-1 ${
@@ -7,16 +8,17 @@ export default function NavLinksDesktop({ isScrolled }) {
 
   return (
     <>
-      <Link href="#hero" className={linkClasses}>Home</Link>
-      <Link href="#products" className={linkClasses}>Products</Link>
-      <Link href="#products" className={linkClasses}>FAQ</Link>
-      <Link href="#about" className={linkClasses}>About Us</Link>
+      <Link href="/" className={linkClasses}>Home</Link>
+      <Link href="/products" className={linkClasses}>Products</Link>
+      <Link href="/faq" className={linkClasses}>FAQ</Link>
+      <Link href="/about" className={linkClasses}>About Us</Link>
       <Link
         href="#footer"
         className="px-4 py-2 rounded-md font-bold bg-green-10 text-white hover:bg-white hover:text-green-10 hover:border-2 border-green-10 transition"
       >
         Contact Us
       </Link>
+      <ThemeSwitch />
     </>
   );
 }
